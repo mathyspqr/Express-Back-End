5efb987c0c2fd313f0dcbefc2ab2018004d9c9e5
Mathys <mathys0@hotmail.fr>
2024-10-30 22:41:50 +0100

    2nd commit

--- api/index.js
+++ api/index.js
@@ -1,44 +1,34 @@
-// Importation des modules nécessaires
 const express = require('express');
 const mysql = require('mysql');
 const cors = require('cors');
 
-// Création de l'application Express
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000; // Utiliser le port défini par Vercel
 
-// Configuration de la connexion à la base de données
 const dbConfig = {
   host: '192.168.1.123', // Remplacez par l'adresse IP de votre serveur MySQL
-  user: 'mathys', // Remplacez par votre nom d'utilisateur MySQL
-  password: 'TiTi60340..', // Remplacez par votre mot de passe MySQL
-  database: 'mathys', // Remplacez par le nom de votre base de données
+  user: 'mathys',
+  password: 'TiTi60340..',
+  database: 'mathys',
 };
 
-// Créer une connexion à la base de données
 const connection = mysql.createConnection(dbConfig);
 
-// Tester la connexion à la base de données
 connection.connect((err) => {
   if (err) {
     console.error('Erreur de connexion à la base de données :', err);
-    process.exit(1); // Arrêter le processus en cas d'erreur
+    return;
   }
   console.log('Connecté à la base de données avec l\'ID', connection.threadId);
 });
 
-// Middleware pour parser le corps des requêtes en JSON
 app.use(express.json());
-
-// Middleware pour gérer les CORS
 app.use(cors());
 
-// Route de test
 app.get('/', (req, res) => {
   res.send('Hello World! Test de connexion à la base de données.');
 });
 
-// Route pour récupérer tous les utilisateurs
 app.get('/users', (req, res) => {
   connection.query('SELECT * FROM users', (err, results) => {
     if (err) {
@@ -48,17 +38,15 @@ app.get('/users', (req, res) => {
   });
 });
 
-
 app.get('/mathys', (req, res) => {
-    connection.query('SELECT * FROM message_serveur', (err, results) => {
-      if (err) {
-        console.error('Erreur SQL:', err); // Affiche l'erreur dans la console
-        return res.status(500).send('Erreur lors de la récupération des données de epsiwis.');
-      }
-      res.json(results);
-    });
+  connection.query('SELECT * FROM message_serveur', (err, results) => {
+    if (err) {
+      console.error('Erreur SQL:', err);
+      return res.status(500).send('Erreur lors de la récupération des données de epsiwis.');
+    }
+    res.json(results);
+  });
 });
-
 
 app.listen(port, () => {
   console.log(`Serveur en cours d'exécution sur http://localhost:${port}`);
