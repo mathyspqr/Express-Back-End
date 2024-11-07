3d93cb59318d76aadad631f14612e7809227a18d
Mathys <mathys0@hotmail.fr>
2024-11-08 00:56:16 +0100

    add comment commit

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql');
 const cors = require('cors');
-// const session = require('express-session');
+const session = require('express-session');
 
 const dbConfig = {
   host: '5.tcp.eu.ngrok.io',
@@ -29,18 +29,17 @@ const corsMiddleware = cors({
 });
 
 // Configurer les sessions
-// const sessionMiddleware = session({
-//   secret: 'mySuperSecretKey12345!@#$', 
-//   resave: false,
-//   saveUninitialized: true,
-//   cookie: { secure: false }
-// });
+const sessionMiddleware = session({
+  secret: 'mySuperSecretKey12345!@#$', 
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+});
 
 // Handler de la fonction serverless
 module.exports = async (req, res) => {
   corsMiddleware(req, res, () => {
-    // sessionMiddleware(req, res, () => {
-      // console.log('Session:', req.session); // Ajoutez ce log pour vérifier l'état de la session
+    sessionMiddleware(req, res, () => {
       if (req.method === 'GET') {
         if (req.url === '/mathys') {
           connection.query('SELECT * FROM message_serveur', (err, results) => {
@@ -60,10 +59,8 @@ module.exports = async (req, res) => {
             res.json(results);
           });
         } else if (req.url.startsWith('/likes/')) {
-          const urlParts = req.url.split('/');
-          const userId = urlParts[2];
-          const messageId = urlParts[3];
-          connection.query('SELECT * FROM likes WHERE user_id = ? AND message_id = ?', [userId, messageId], (err, results) => {
+          const messageId = req.url.split('/')[2];
+          connection.query('SELECT * FROM likes WHERE message_id = ?', [messageId], (err, results) => {
             if (err) {
               console.error('Erreur SQL lors de la récupération des likes :', err.code, err.sqlMessage);
               return res.status(500).json({ error: 'Erreur lors de la récupération des likes.' });
@@ -71,12 +68,11 @@ module.exports = async (req, res) => {
             res.json(results);
           });
         } else if (req.url === '/session') {
-          // if (req.session.user) {
-          //   res.send(`Utilisateur connecté : ${req.session.user.username}`);
-          // } else {
-          //   res.status(401).json({ error: 'Aucun utilisateur connecté' });
-          // }
-          res.status(404).json({ error: 'Route non trouvée.' });
+          if (req.session.user) {
+            res.send(`Utilisateur connecté : ${req.session.user.username}`);
+          } else {
+            res.status(401).json({ error: 'Aucun utilisateur connecté' });
+          }
         } else {
           res.status(404).json({ error: 'Route non trouvée.' });
         }
@@ -122,12 +118,10 @@ module.exports = async (req, res) => {
           });
         } else if (req.url.startsWith('/like-message/')) {
           const messageId = req.url.split('/')[2];
-          // if (!req.session.user) {
-          //   console.log('Utilisateur non connecté'); // Ajoutez ce log pour vérifier l'état de la session
-          //   return res.status(401).json({ error: 'Utilisateur non connecté' });
-          // }
-          // const userId = req.session.user.id;
-          const userId = 1; // Utilisateur fictif pour le test
+          if (!req.session.user) {
+            return res.status(401).json({ error: 'Utilisateur non connecté' });
+          }
+          const userId = req.session.user.id;
           connection.query('INSERT INTO likes (user_id, message_id) VALUES (?, ?)', [userId, messageId], (err, results) => {
             if (err) {
               console.error('Erreur SQL lors de l\'ajout du like :', err.code, err.sqlMessage);
@@ -169,7 +163,7 @@ module.exports = async (req, res) => {
                   return res.status(500).json({ error: 'Erreur lors de la connexion de l\'utilisateur.' });
                 }
                 if (results.length > 0) {
-                  // req.session.user = { id: results[0].id, username };
+                  req.session.user = { id: results[0].id, username };
                   res.status(200).json({ message: 'Connexion réussie.' });
                 } else {
                   res.status(401).json({ error: 'Nom d\'utilisateur ou mot de passe incorrect.' });
@@ -200,6 +194,6 @@ module.exports = async (req, res) => {
       } else {
         res.status(404).json({ error: 'Route non trouvée.' });
       }
-    // });
+    });
   });
 };
